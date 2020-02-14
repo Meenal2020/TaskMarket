bab85db5d0f62eaf797c2235744019ce9fd5a355
Tamas Szirbucz <szirbucz@kodekonveyor.com>
2020-02-14 14:07:34 +0100

    Avoid opening Firefox window by open it in headless mode.

--- src/test/java/com/kodekonveyor/market/integrationtests/SeleniumTestHelper.java
+++ src/test/java/com/kodekonveyor/market/integrationtests/SeleniumTestHelper.java
@@ -3,6 +3,7 @@ package com.kodekonveyor.market.integrationtests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -11,15 +12,17 @@ class SeleniumTestHelper {
   private static FirefoxDriver driver;
 
   static {
-    driver = new FirefoxDriver();
-  }
-
-  public static FirefoxDriver getDriver() {
-    return driver;
+    final FirefoxOptions firefoxOptions = new FirefoxOptions();
+    firefoxOptions.addArguments("--headless");
+    driver = new FirefoxDriver(firefoxOptions);
   }
 
   public static void close() {
     driver.close();
+  }
+
+  public static FirefoxDriver getDriver() {
+    return driver;
   }
 
   public static WebElement waitFor(final String cssSelector) {
